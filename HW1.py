0401f0c804f8bd78af620ea8bf01c602aa03abf6
SamuelRaymond <sr128226@student.wpsok.org>
2025-08-20 12:57:51 -0500

    HW2

--- HW1.py
+++ HW1.py
@@ -1,5 +1,20 @@
 #SAMUEL RAYMOND
 #HW1
 #5th Hour
+import random
 
+a = 1
+b = random.randint(1,3)
 print("hello world")
+print(a)
+while a > 0 :
+    input()
+    b += 1
+    print(b)
+
+
+
+
+
+
+
---
+++ HW2.py
@@ -0,0 +1,32 @@
+#Name: samuel
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("hello world")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+sam = 5
+aiden = "no"
+gage = False
+#3. Print all three variables on the same print function (at the same time).
+print(sam, aiden, gage)
+#4. Create a variable that asks the user to input an integer.
+intup = int(input())
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+anser = sam + intup
+print(anser)
+#6. Take the result from #5 and divide it by 2. Print the result.
+ban = anser / 2
+print(ban)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+if gage == True :
+    gage = False
+else:
+    gage = True
+#8. Print the value of the boolean variable.
+print(gage)
+#9. Create a variable with a number that contains decimals.
+eat = 6.6
+#10. Round the number from #9 up or down using the round function.
+print(round(6.6))
