04f893e3e5e3b98f7162c1b7793b42da584dc220
SamuelRaymond <sr128226@student.wpsok.org>
2025-11-04 12:36:01 -0600

    HW12

--- HIGHER_OR_LOWER.py
+++ HIGHER_OR_LOWER.py
@@ -1,18 +1,18 @@
-import random
-random_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-card = random.choice(random_list)
-print(card)
-while 5 > 3:
-    card = random.choice(random_list)
-    int(input())
-    x = card
-    random_list.remove(card)
-    if card == 13:
-        print("king")
-    elif card == 12:
-        print("queen")
-    elif card == 11:
-        print("Jack")
-    else :
-        print(card)
-    random_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+
+
+
+
+
+
+
+
+print ("enter your name")
+name = input()
+print ("do you want to play a game of choice? (yes/no)")
+choice = input()
+if choice == "yes":
+x = input()
+if x == "Yes":
+    print("you have started the ")
---
+++ HW12.py
@@ -0,0 +1,39 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW12
+import random
+#1. Create a while loop with variable i that counts down from 5 to 0 and then prints
+#"Hello World!" at the end.
+i = 5
+while i > -1 :
+    print (i)
+    i -= 1
+print ("hello world")
+#2. Create a while loop that prints only even numbers between 1 and 30 (HINT: modulo).
+num = 0
+while num < 30 :
+    num += 1
+    if num % 2 == 0 :
+        print (num)
+
+#3. Create a while loop that continues (skips the number) if the number is divisible by 3.
+mun = 0
+while mun < 30 :
+    mun += 1
+    if mun % 3 == 0 :
+        continue
+    else:
+        print (mun)
+#4. Create a while loop that randomly generates a number between 1 and 6, prints the result,
+#and then breaks the loop if it's a 1.
+rand = 7
+while rand > 1 :
+    rand = random.randint(1,6)
+    print (rand)
+    if rand == 1 :
+        break
+#5. Create a while loop that asks for a number input until the user inputs the number 0.
+g = int(input())
+while not g == 0 :
+    print ("try again")
+    g = int(input())
