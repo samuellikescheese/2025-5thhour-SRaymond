a22b696e65f5fd89000fab59291e0cbfbc289c31
Samuel Raymond <sr128226@student.wpsok.org>
2026-01-15 13:11:10 -0600

    here ya go

---
+++ HW16.py
@@ -0,0 +1,34 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW16
+import random
+#1. Create a def function that prints out "Hello World!"
+def me():
+    print ("Hello world!")
+#2. Create a def function that calculates the average of three numbers (set the 3 numbers as your arguments).
+def avg(a, b, c):
+    d = (a + b + c) / 3
+    print(d)
+#3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
+#prints the name of the third animal.
+def ani(*animal):
+    print (animal[2])
+#4. Create a def function that loops from 1 to the number put in the argument.
+def num(number):
+    for i in range(1, number + 1):
+        print (i)
+#5. Call all of the functions created in 1 - 4 with relevant arguments.
+me()
+avg(4,6,2)
+ani("monkey", "ant", "elephant", "pony", "gorilla")
+num(6)
+#6. Create a variable x that has the value of 2. Print x
+x = 2
+print (x)
+#7. Create a def function that multiplies the value of 2 by a random number between 1 and 5.
+def exr():
+    global x
+    x = x * random.randint(1,5)
+exr()
+#8. Print the new value of x.
+print (x)
