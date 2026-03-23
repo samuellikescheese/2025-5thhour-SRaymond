bbc8fad89e50c9d1f53fb3664306ea83c19f0963
Samuel Raymond <sr128226@student.wpsok.org>
2026-03-23 13:00:18 -0500

    here ya go

---
+++ RV1.py
@@ -0,0 +1,34 @@
+#Name:samuel raymond
+#Class: 5th Hour
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+print ("Hello World!")
+#2. Create three variables with strings as values.
+a1 = ("sam")
+b1 = ("aiden")
+c1 = ("gage")
+#3. Create three variables with booleans as values.
+a2 = True
+b2 = False
+c2 = True
+#4. Create three variables with integers as values.
+a3 = 5
+b3 = 314
+c3 = 3
+#5. Create a variable that asks for the user's name and prints it.
+sam = input()
+print(sam)
+#6. Add together the three variables from #4 and print the result.
+mult = a3 + b3 + c3
+print (mult)
+#7. Subtract the second variable from the first from #4 and print the result.
+print(a3 - c3)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+print (mult * c3)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+cheese = float(input("give me a decimal"))
+#10. Round the variable from #9 to the nearest whole number and print the result.
+print (round(cheese))
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+print (355 % 113)
