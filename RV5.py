b75cf012cb06154872c9534b350ce56482415765
Samuel Raymond <sr128226@student.wpsok.org>
2026-04-06 12:53:47 -0500

    here ya go

---
+++ RV5.py
@@ -0,0 +1,14 @@
+#Name:samuel
+#Class: 5th Hour
+#Assignment: HW-R5
+
+#1. Create a list of the names of all the students in the classroom.
+listerc = ["samuel","aiden","dylan","ivan","hogan","brenlyn","ashton","bryson"]
+#2. Create a for loop that prints the names of every student in the list.
+for i in listerc:
+    print (i)
+#3. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it.
+for i in listerc:
+    if "e" in i:
+        print (i)
